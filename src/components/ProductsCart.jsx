c83e3c201b55d5bc9212418aef0b96df427b0aee
Martyna <m.smolarek19@gmail.com>
2022-03-05 19:07:29 +0100

    added map to contact page

--- src/components/ProductsCart.jsx
+++ src/components/ProductsCart.jsx
@@ -1,8 +1,3 @@
-import styled from 'styled-components';
-import { products } from '../data';
-import { Delete } from '@material-ui/icons';
-import { useState } from 'react';
-import CartReturned from './CartItemSingleAdded';
 import CartItemSingleAdded from './CartItemSingleAdded';
 import { SharedParagraph } from '../styling/sharedStyling';
 import { CartProductsSummaryContainer, CartProductsSummaryItems } from '../styling/userCartStyling';
